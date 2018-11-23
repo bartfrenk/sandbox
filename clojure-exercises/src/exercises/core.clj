ae5b676877952840f5283d1d85b1f4b83099df4e
Bart Frenk <bart.frenk@gmail.com>
2018-11-23 17:10:38 +0100

    Add some 4clojure exercises

--- clojure-exercises/src/exercises/core.clj
+++ clojure-exercises/src/exercises/core.clj
@@ -57,21 +57,96 @@
          (into #{}))))
 
 
-(conj ) (conj nil 1)
 
-(equiv #(* % %) #{-2 -1 0 1 2})
+;; Problem 105
+(def collect
+  (fn [xs]
+    (letfn [(update* [m k f & more]
+              (assoc m k (apply f (get m k) more)))
+            (collect* [[m active] x]
+              (if (keyword? x)
+                [(assoc m x []) x]
+                [(update* m active conj x) active]))]
+      (first (reduce collect* [{} nil] xs)))))
+
+(def collect-2
+  #(->> (partition-by keyword? %)
+        (partition 2)
+        (reduce (fn [agg [k v]]
+                  (-> (zipmap (reverse k) (cons v (repeat ())))
+                      (into agg))) {})))
+
+(def collect-3
+  #(->> (partition-by keyword? %)
+        (partition 2)
+        (mapcat (fn [[kws xs]]
+               (->> (butlast kws)
+                    (map (fn [kw] [kw []]))
+                    ((fn [ys]
+                       (conj ys [(last kws) (vec xs)]))))))
+        (into {})))
+
+(def t (flatten (mapcat identity {:a (range 100000) :b (range 100000)})))
+(time (doall (collect t))) ;; around 135 msecs
+(time (doall (collect-2 t))) ;; around 60 msecs
+(time (doall (collect-3 t))) ;; around 70 msecs
+(collect-3  [:a 1 2 3 :b :c 4])
+
+;; Problem 137
+(def digits
+  (fn [n base]
+    (loop [acc [] m n]
+      (cond
+        (zero? m) (if (empty? acc) [0] (reverse acc))
+        :else (recur (conj acc (mod m base)) (quot m base))))))
+
+(digits 1234501 10)
+(digits 0 11)
+(digits 9 2)
+(time (digits Integer/MAX_VALUE 42))
+
+;; Problem 144
+(def oscilrate
+  (fn oscilrate* [val & fns]
+    (letfn [(it
+              ([val] (list val))
+              ([val f & fns]
+               (lazy-seq (cons val (apply oscilrate* (f val) fns)))))]
+      (apply it val (cycle fns)))))
+
+(take 10 (oscilrate 3.14 int double))
+(take 5 (oscilrate 3 #(- % 3) #(+ 5 %)))
+
+(take 10 (cycle [1 2 3]))
 
 
-(concat #{1 2} #{3 4})
+;; Problem 110
+(def pronunciation
+  (fn [zs]
+    (letfn [(pronounce [xs]
+              (->> (partition-by identity xs)
+                   (mapcat (fn [xss] [(count xss) (first xss)]))))]
+      (iterate pronounce (pronounce zs)))))
 
-(power-set* #{1 2})
 
-(empty? x)
+(def uncurry
+  (fn [g]
+    (letfn [(step [f xs]
+              (if (fn? f)
+                (recur (f (first xs)) (rest xs))
+                f))]
+      (fn [& args]
+        (step g args)))))
 
-(def x #{1 2})
 
-(disj x 1)
+(uncurry (fn [a]
+           (fn [b]
+             (fn [c]
+               (fn [d]
+                 (+ a b c d)))))
+         [1 2 3 4])
 
-(conj x 3)
 
-(concat)
+(take 3 (pronunciation [1]))
+
+(partition-by identity [1 1 2])
