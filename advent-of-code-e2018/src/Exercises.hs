2e77577a289ff05523fb25a9006035cc85b5f27c
Bart Frenk <bart.frenk@gmail.com>
2018-12-02 22:29:30 +0100

    Do day 2 exercises of advent of code 2018

--- advent-of-code-e2018/src/Exercises.hs
+++ advent-of-code-e2018/src/Exercises.hs
@@ -6,6 +6,8 @@ import Control.Monad.State
 import Data.List (foldl')
 import Data.Functor.Identity
 import Text.Parsec
+import qualified Data.Map.Strict as Map
+import Data.Map.Strict (Map)
 import qualified Data.Set as Set
 
 type Parser s a = Parsec s () a
@@ -40,4 +42,50 @@ answer1a = runExceptT (foldl' (+) 0 <$> readChanges "res/frequency-changes.txt")
 answer1b :: IO  (Either ParseError Int)
 answer1b = runExceptT (findFirstDuplicate <$> readChanges "res/frequency-changes.txt")
 
+readLines :: FilePath -> IO [String]
+readLines path = lines <$> readFile path
 
+score :: String -> (Int, Int)
+score cs =
+  let m = count Map.empty cs
+      ns = snd <$> (Map.toList m)
+  in (if 2 `elem` ns then 1 else 0, if 3 `elem` ns then 1 else 0)
+  where
+    count m [] = m
+    count m (c:css) = count (Map.insertWith (+) c 1 m) css
+
+
+answer2a :: IO Int
+answer2a = do
+  lines <- readLines "res/letters.txt"
+  let scores = score <$> lines
+  let (twos, threes) = foldl' sumPairs (0, 0) scores
+  pure $ twos * threes
+  where
+    sumPairs (x, y) (u, v) = (x + u, y + v)
+
+
+answer2b :: IO String
+answer2b = do
+  lines <- readLines "res/letters.txt"
+  let (x, y) = head $ connected lines
+  pure $ remove x y
+  where
+    remove (c:cs) (d:ds) = if c == d then (c:remove cs ds) else remove cs ds
+    remove _ _ = []
+
+connected :: [String] -> [(String, String)]
+connected zs = do
+  x <- zs
+  y <- zs
+  if dist x y == 1 then [(x, y)] else []
+
+dist :: String -> String -> Int
+dist s t = dist' 0 s t
+  where
+    dist' acc [] _ = acc
+    dist' acc _ [] = acc
+    dist' !acc (c:cs) (d:ds) =
+      if c == d
+      then dist' acc cs ds
+      else dist' (acc + 1) cs ds
