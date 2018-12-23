184ea932c16369a5af9eca98049f5404305586b8
Bart Frenk <bart.frenk@gmail.com>
2018-12-23 22:06:45 +0100

    Solve day16

--- advent-of-code-2018/haskell/app/day16/Main.hs
+++ advent-of-code-2018/haskell/app/day16/Main.hs
@@ -38,13 +38,13 @@ setOpCode :: b -> Instruction a -> Instruction b
 setOpCode b = fmap (\_ -> b)
 
 data Sample = Sample
-  { input       :: Vector Int
-  , instr :: Instruction Int
-  , output      :: Vector Int
+  { input       :: Vector Integer
+  , instr       :: Instruction Int
+  , output      :: Vector Integer
   } deriving (Show)
 
 -- |Parser for numbers.
-number :: Parsec String () Int
+number :: Read a => Parsec String () a
 number = read <$> many1 digit
 
 -- |Parser for encoded instructions (with instruction numbers instead of actual
@@ -83,37 +83,37 @@ parseInput = liftEither . runParser parser () ""
     sampleList = manyTill (sample <* newline) $ void (many1 newline)
     instructions = many instruction
 
-exec :: Instruction OpCode -> Vector Int -> Vector Int
+exec :: Instruction OpCode -> Vector Integer -> Vector Integer
 exec (Instruction ADDR in1 in2 out) input =
   input // [(out, input ! in1 + input ! in2)]
 exec (Instruction ADDI in1 in2 out) input =
-  input // [(out, input ! in1 + in2)]
+  input // [(out, input ! in1 + toInteger in2)]
 exec (Instruction MULR in1 in2 out) input =
   input // [(out, input ! in1 * input ! in2)]
 exec (Instruction MULI in1 in2 out) input =
-  input // [(out, input ! in1 * in2)]
+  input // [(out, input ! in1 * toInteger in2)]
 exec (Instruction BANR in1 in2 out) input =
   input // [(out, input ! in1 .&. input ! in2)]
 exec (Instruction BANI in1 in2 out) input =
-  input // [(out, input ! in1 .&. in2)]
+  input // [(out, input ! in1 .&. toInteger in2)]
 exec (Instruction BORR in1 in2 out) input =
   input // [(out, input ! in1 .|. input ! in2)]
 exec (Instruction BORI in1 in2 out) input =
-  input // [(out, input ! in1 .|. in2)]
+  input // [(out, input ! in1 .|. toInteger in2)]
 exec (Instruction SETR in1 _ out) input =
   input // [(out, input ! in1)]
 exec (Instruction SETI in1 _ out) input =
-  input // [(out, in1)]
+  input // [(out, toInteger in1)]
 exec (Instruction GTIR in1 in2 out) input =
-  input // [(out, if in1 > input ! in2 then 1 else 0)]
+  input // [(out, if toInteger in1 > input ! in2 then 1 else 0)]
 exec (Instruction GTRI in1 in2 out) input =
-  input // [(out, if input ! in1 > in2 then 1 else 0)]
+  input // [(out, if input ! in1 > toInteger in2 then 1 else 0)]
 exec (Instruction GTRR in1 in2 out) input =
   input // [(out, if input ! in1 > input ! in2 then 1 else 0)]
 exec (Instruction EQIR in1 in2 out) input =
-  input // [(out, if in1 == input ! in2 then 1 else 0)]
+  input // [(out, if toInteger in1 == input ! in2 then 1 else 0)]
 exec (Instruction EQRI in1 in2 out) input =
-  input // [(out, if input ! in1 == in2 then 1 else 0)]
+  input // [(out, if input ! in1 == toInteger in2 then 1 else 0)]
 exec (Instruction EQRR in1 in2 out) input =
   input // [(out, if input ! in1 == input ! in2 then 1 else 0)]
 
@@ -131,9 +131,10 @@ listAmbiguousSamples n samples = isAmbiguous `filter` samples
     isAmbiguous s = length (possibleOpCodes s) >= n
 
 -- |Find the possible encodings from number to actual op codes, that match the
--- samples.
-findPossibleEncodings :: [Sample] -> Map Int [OpCode]
-findPossibleEncodings = foldl' restrict Map.empty
+-- samples, without taking into account that each instruction number needs to
+-- map to a single op code.
+findCoherentEncodings :: [Sample] -> Map Int [OpCode]
+findCoherentEncodings = foldl' restrict Map.empty
   where
     restrict :: Map Int [OpCode] -> Sample -> Map Int [OpCode]
     restrict enc sample@(Sample _ inst _) =
@@ -141,15 +142,27 @@ findPossibleEncodings = foldl' restrict Map.empty
       in Map.insertWith intersect (getOpCode inst) encodings enc
     getOpCode (Instruction op _ _ _) = op
 
--- |Select the first of possible encodings. Return Nothing if there is at least
--- one instruction number that does not have a an op code that matches with the
--- sample.
-selectPossibleEncoding :: [Sample] -> Maybe (Map Int OpCode)
-selectPossibleEncoding samples =
-  let possibleEncodings = findPossibleEncodings samples
-  in flip traverse possibleEncodings $ \case
-    [] -> Nothing
-    (op:_) -> Just op
+-- |Select the coherent encodings that also takes into account that different
+-- instruction numbers need to map to different op codes. Return 'Nothing' if no
+-- such encoding exists.
+findSingleEncoding :: [Sample] -> Maybe (Map Int OpCode)
+findSingleEncoding samples =
+  let initial = Map.toList $ findCoherentEncodings samples
+  in traverse select $ Map.fromList (recur initial)
+
+  where
+    recur :: [(Int, [OpCode])] -> [(Int, [OpCode])]
+    recur enc =
+      let found = ((==) 1 . length . snd) `filter` enc
+          exclude = concat (snd <$> found)
+      in if length found == length enc
+      then found
+      else recur $ flip fmap enc $
+           \(n, candidates) -> if length candidates == 1
+                               then (n, candidates)
+                               else (n, candidates \\ exclude)
+    select [e] = Just e
+    select _ = Nothing
 
 main :: IO ()
 main = putStrLn "Day 16"
@@ -157,6 +170,9 @@ main = putStrLn "Day 16"
 contents :: IO String
 contents = readFile "res/input-16.txt"
 
+samples :: IO [Sample]
+samples = (runExceptT $ liftIO contents >>= parseSampleList) >>= \case
+    Right samples -> pure $ samples
 
 x :: IO (Either ParseError Sample)
 x = runExceptT $ parseSample sampleString1
@@ -166,14 +182,28 @@ y = runExceptT $ possibleOpCodes <$> parseSample sampleString2
 
 z :: IO (Maybe (Map Int OpCode))
 z = (runExceptT $ liftIO contents >>= parseSampleList) >>= \case
-    Right samples -> pure $ selectPossibleEncoding samples
+    Right samples -> pure $ findSingleEncoding samples
 
 u :: IO (Either ParseError [Instruction Int])
 u = (runExceptT $ snd <$> (liftIO contents >>= parseInput))
 
+coherentEncodings :: IO (Map Int [OpCode])
+coherentEncodings = (runExceptT $ liftIO contents >>= parseSampleList) >>= \case
+    Right samples -> pure $ findCoherentEncodings samples
+
 answerA :: IO Int
 answerA = (runExceptT $ liftIO contents >>= parseSampleList) >>= \case
     Right samples -> pure $ length (listAmbiguousSamples 3 samples)
+
+answerB :: IO (Either ParseError Integer)
+answerB = runExceptT $ do
+  (samples, instructions) <- liftIO contents >>= parseInput
+  case findSingleEncoding samples of
+    Just enc -> pure $
+      let instructions' = fmap ((Map.!) enc) <$> instructions
+      in (foldl' (flip exec) input instructions') ! 0
+  where
+    input = Vector.fromList [0, 0, 0, 0]
 
 sampleString1 = unlines
   [ "Before: [1, 1, 1, 0]"
