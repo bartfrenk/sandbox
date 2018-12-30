a35d273eab8fae9989a9bf37a3c119330d15b69f
Bart Frenk <bart.frenk@gmail.com>
2018-12-30 01:00:11 +0100

    Finish day 17 of advent of code

--- advent-of-code-2018/haskell/app/day17/Main.hs
+++ advent-of-code-2018/haskell/app/day17/Main.hs
@@ -1,4 +1,13 @@
+-- Essentially a recursive solution, but awkwardly encoded using a list of
+-- active points, instead of letting the runtime keep track of the work still to
+-- be done.
+
+-- Works, but not too happy with the final form. Maybe rewrite in actual
+-- recursive form for comparison.
+
 {-# LANGUAGE ScopedTypeVariables #-}
+import           Prelude                     hiding (readFile)
+
 import           Control.Monad
 import           Control.Monad.Except
 import           Control.Monad.Primitive     (PrimMonad, PrimState)
@@ -9,12 +18,21 @@ import qualified Data.Vector.Generic         as G
 import qualified Data.Vector.Generic.Mutable as GM
 import qualified Data.Vector.Unboxed         as U
 import qualified Data.Vector.Unboxed.Mutable as UM
+import           System.IO.Strict            (readFile)
 import           Text.Parsec                 hiding (State)
 
 import           Debug.Trace
 
 main :: IO ()
-main = putStrLn "day 17"
+main = readFile "res/input-17.txt" & fmap parseGround >>= \case
+  Left err -> putStrLn $ show err
+  Right init -> do
+    let irrigated = evolve init [(500, 0)]
+    -- The -7 correction is to avoid counting the water squares that lie above
+    -- the tip of the highest basin (i.e., have y-coordinate smaller than any
+    -- y-coordinate appearing in the input data).
+    putStrLn $ "Part A: " ++ show ((countWater "~|" irrigated) - 7)
+    putStrLn $ "Part B: " ++ show (countWater "~" irrigated)
 
 -- |Representation of points in the ground to be irrigated.
 type Point = (Int, Int)
@@ -23,6 +41,7 @@ type Point = (Int, Int)
 data Ground v = Ground
   { squares :: v Char
   , bounds  :: Block
+  , minY :: Int
   }
 
 instance Show (Ground U.Vector) where
@@ -36,57 +55,89 @@ instance Show (Ground U.Vector) where
         | otherwise = let (segment, remains) = U.splitAt n xs
                       in (segment:chunkVector n remains)
 
-
-
--- |Simple wrapper to indicate the state of the irrigation process.
-data State v = State
-  { ground :: Ground v
-  , active :: [Point]
-  }
-
 -- |Takes the active water square at the head of the list of active squares, and
 -- fills the appropriate surrounding squares with water, depending only on the
 -- neighborhood of the active square. Prefixes points to the active list.
 stepM :: (PrimMonad m, GM.MVector v Char)
       => Ground (v (PrimState m)) -> [Point] -> m [Point]
 stepM g [] = pure []
-stepM g active@(p:rest) = do
-  switchM
-    [ (peek here '+') |&& (peek down '.'), (put down '|') >> pure ((down p):active)
+stepM g active@(center:rest) = do
+  let down = move (0, 1)
+      up = move (0, -1)
+      left = move (-1, 0)
+      right = move (1, 0)
+  downC <- peek down
+  upC <- peek up
+  leftC <- peek left
+  rightC <- peek right
+  centerC <- peek center
+  if | centerC === '+' && downC === '.' ->
+       put down '|' >> pure (down:active)
 
-      ((g <!?> p `isM` '+') `andM` (g <!?> (down p) `isM` '.'),
-       put g (down p) '|' >> pure (down p:active))
+     | centerC === '+' && downC === '|' ->
+       pure rest
 
-    , ((g <!?> p `isM` '|') `andM` (g <!?> (down p) `isM` '.'),
-       put g (down p) '|' >> pure (down p:active))
+     | centerC === '|' && downC === '.' ->
+       put down '|' >> pure (down:active)
 
-    , ((g <!?> p `isM` '|') `andM` (g <!?> (down p) `isM` '#'),
-       pure rest)
+     | centerC === '|' && (downC == Nothing || downC === '|') ->
+       pure rest
 
-    , (pure True, pure [])
+     | centerC === '|' && (leftC === '~' || leftC === '|' ||
+                           rightC === '|' || rightC === '~') -> do
+         fromLeft <- if
+           | leftC === '~' || leftC === '.' -> put left '|' >> pure [left]
+           | otherwise -> pure []
+         fromRight <- if
+           | rightC === '~' || rightC === '.' -> put right '|' >> pure [right]
+           | otherwise -> pure []
+         pure $ fromLeft ++ fromRight ++ rest
 
-    , ((peek here '|') |&& (peek down '.'), pure [])
-    ]
+     | centerC === '|' && (downC === '#' || downC === '~') -> do
+         fromLeft <- if
+           | leftC === '.' || leftC === '|' || leftC === '~' -> put left '~' >> pure [left]
+           | otherwise -> pure []
+         fromRight <- if
+           | rightC === '.' || rightC === '|' || rightC === '~' -> put right '~' >> pure [right]
+           | otherwise -> pure []
+         put center '~'
+         pure $ fromLeft ++ fromRight ++ rest
+
+     | centerC === '~' && (downC === '#' || downC === '~') -> do
+         fromLeft <- if
+           | leftC === '.' || leftC === '|' -> put left '~' >> pure [left]
+           | otherwise -> pure []
+         fromRight <- if
+           | rightC === '.' || rightC === '|' -> put right '~' >> pure [right]
+           | otherwise -> pure []
+         pure $ fromLeft ++ fromRight ++ rest
+
+     | centerC === '~' && downC === '.' -> do
+         put down '|'
+         pure $ (down:active)
+
+     | centerC === '~' && downC === '|' -> do
+         fromLeft <- if
+           | leftC === '~' -> put left '|' >> pure [left]
+           | otherwise -> pure []
+         fromRight <- if
+           | rightC === '~' -> put right '|' >> pure [right]
+           | otherwise -> pure []
+         put center '|'
+         pure $ [center] ++ fromLeft ++ fromRight ++ rest
+
+     | otherwise -> pure rest
+
   where
-    here = id
-    peek loc c = (g <!?> (loc p)) `isM` c
-    down (x, y) = (x, y + 1)
-    isM a b = (== (Just b)) <$> a
-    andM = liftM2 (&&)
-    (|&&) = andM
-    whenM cond act = do
-      b <- cond
-      if b then act else pure ()
+    move (u, v) = (u + fst center, v + snd center)
+    peek q = g <!?> q
+    put q c = writeSquare g q c
+    (===) (Just a) b = a == b
+    (===) Nothing _ = False
 
-switchM :: Monad m => [(m Bool, m a)] -> m a
-switchM [] = error "No condition matches"
-switchM ((x, a):xs) = do
-  b <- x
-  if b then a else switchM xs
-
-put :: (PrimMonad m, GM.MVector v Char)
+writeSquare :: (PrimMonad m, GM.MVector v Char)
     => Ground (v (PrimState m)) -> (Int, Int) -> Char -> m ()
-put Ground{..} p c =
+writeSquare Ground{..} p c =
   let idx = toIndex bounds p
   in GM.write squares idx c
 
@@ -98,24 +149,21 @@ Ground{..} <!?> p =
      then pure $ Nothing
      else Just <$> GM.unsafeRead squares idx
 
--- process ground p
---   | ground <!> p == '+' && ground <!> below p = Just '.' put ground (below p) '|'
-
-evolveN :: forall v. G.Vector v Char => Int -> Ground v -> [Point] -> Ground v
-evolveN n ground active = Ground squares' (bounds ground)
+-- |Run stepM until no active points remain.
+evolve :: forall v. G.Vector v Char => Ground v -> [Point] -> Ground v
+evolve ground active = Ground squares' (bounds ground) (minY ground)
   where
-    update msquares =
-      void (loopM n (stepM $ Ground msquares (bounds ground)) active)
-    squares' =  G.modify update (squares ground)
+    update ps msquares = do
+      ps' <- stepM (Ground msquares (bounds ground) (minY ground)) ps
+      if null ps' then pure ()
+      else update ps' msquares
+    squares' =  G.modify (update active) (squares ground)
 
--- |Variant of 'iterateM' that stops after 'n' iterations.
-loopM :: Monad m => Int -> (a -> m a) -> a -> m a
-loopM n act v = recur n v
-  where
-    recur n' v'
-      | n' == 0 = pure v'
-      | n' > 0 = act v' >>= recur (n' - 1)
-      | otherwise = error "Iteration count needs to be positive"
+-- |Count the number of water squares (e.g., the squares in 'water').
+countWater :: G.Vector v Char => String -> Ground v -> Int
+countWater water (Ground {squares}) =
+  G.length (G.filter (`elem` water) squares)
+
 
 -- === PARSING ===
 
@@ -124,12 +172,12 @@ data Block = Block
   , y :: (Int, Int)
   } deriving (Show)
 
-parseGround :: Monad m => String -> ExceptT ParseError m (Ground U.Vector)
-parseGround s = liftEither $ do
+parseGround :: String -> Either ParseError (Ground U.Vector)
+parseGround s = do
   blocks <- runParser (endBy block newline) () "" s
-  let bounds = computeBounds blocks
+  let (bounds, minY) = computeBounds blocks
   let v = U.modify (putBlocks bounds blocks) (U.replicate (size bounds) '.')
-  pure $ Ground v bounds
+  pure $ Ground v bounds minY
   where
     horizontal = do
       _ <- string "y="
@@ -170,15 +218,14 @@ toIndex (Block (a, b) (c, _)) (x, y) =
 -- |Compute the bounds of the map that we need to take into consideration to
 -- solve the puzzle. Leaves two lines at the left and right edges, none at top and
 -- bottom.
-computeBounds :: [Block] -> Block
+computeBounds :: [Block] -> (Block, Int)
 computeBounds [] = error "No blocks"
 computeBounds (block:rest) = expand $ foldl' coverBlock block rest
   where
     coverBlock (Block x y) (Block u v) =
       Block (coverRange x u) (coverRange y v)
     coverRange (a, b) (c, d) = (min a c, max b d)
-    expand (Block (lo, hi) (_, r)) = Block (lo - 1, hi + 1) (0, r)
-
+    expand (Block (lo, hi) (minY, r)) = (Block (lo - 1, hi + 1) (0, r), minY)
 
 sample :: String
 sample = unlines
@@ -191,17 +238,3 @@ sample = unlines
   , "x=504, y=10..13"
   , "y=13, x=498..504"
   ]
-
-fromRight :: Either a b -> b
-fromRight (Right b) = b
-
-t :: IO ()
-t = do
-  ground <- fromRight <$> (runExceptT $ parseGround sample)
-  print $ evolveN 7 ground [(500, 0)]
-
--- Approach:
-
--- Use a vector, keep active points, and expand the active points based on local
--- rules. Since this is mainly doing updates, and many of them, I think we
--- a mutable vector is appropriate.
