f924517d165a14070be29be34ce91b36d8a61def
Bart Frenk <bart.frenk@gmail.com>
2019-07-02 23:13:29 +0200

    Rethink yaml-conf

--- yaml-conf/src/Data/Config/Internals.hs
+++ yaml-conf/src/Data/Config/Internals.hs
@@ -1,11 +1,12 @@
 module Data.Config.Internals where
 
-import           Control.Monad.Catch  (Exception, MonadThrow, throwM)
+import           Control.Monad.Catch       (Exception, MonadThrow, throwM)
 import           Control.Monad.Trans
-import           Data.ByteString.Lazy (ByteString)
+import           Control.Monad.Trans.Maybe
+import           Data.ByteString.Lazy      (ByteString)
 import           Data.Maybe
 import           Data.YAML
-import           Data.YAML.Event      (Tag, mkTag)
+import           Data.YAML.Event           (Tag, mkTag)
 
 newtype ParseError = ParseError String deriving (Eq, Show)
 
@@ -34,20 +35,26 @@ test = decodeConfig example
 
 -- Not going to work, since we might need to query the environment to know whether
 -- a value exists
-type Resolver m = (Node -> m Node) -> Node -> Maybe (m Node)
+type Resolver m = (Node -> m (Maybe Node)) -> Node -> m (Maybe Node)
 
--- orResolver :: Monad m => Resolver m
--- orResolver cont node@(Sequence tag nodes)
---   | tag == mkTag "!or" = Just $ pure $ head (cont <$> nodes)
---   | otherwise = Nothing
+orResolver :: Monad m => Resolver m
+orResolver cont node@(Sequence tag nodes)
+  | tag == mkTag "!or" = head <$> cont `mapM` nodes
+  | otherwise = pure Nothing
+  where
+    firstValue [] = undefined
 
 baseResolver :: Monad m => Resolver m
-baseResolver cont node@(Scalar _) = Just $ pure node
-baseResolver cont (Mapping tag mapping) = Just $ Mapping tag <$> cont `mapM` mapping
-baseResolver cont (Sequence tag nodes) = Just $ Sequence tag <$> cont `mapM` nodes
-baseResolver _ node = Just $ pure node
+baseResolver cont node@(Scalar _) = pure $ Just node
+baseResolver cont (Mapping tag mapping) =
+  Just . Mapping tag . collapse <$> cont `mapM` mapping
+baseResolver cont (Sequence tag nodes) =
+  Just . Sequence tag . collapse <$> cont `mapM` nodes
+baseResolver _ node = pure $ Just node
 
-resolve :: MonadThrow m => Resolver m -> Node -> m Node
-resolve res node = fromMaybe
-  (throwM $ ParseError "Failed to parse")
-  (res (resolve res) node)
+resolve :: MonadThrow m => Resolver m -> Node -> m (Maybe Node)
+resolve res node = res (resolve res) node
+
+-- Generalizes catMaybes
+collapse :: Foldable f => f (Maybe a) -> f a
+collapse = undefined
---
+++ yaml-conf/src/Data/Config/Scratch.hs
@@ -0,0 +1,3 @@
+module Data.Config.Scratch where
+
+
