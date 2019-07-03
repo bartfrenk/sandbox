df0956b749ebe78049a9d3e03ed4c781bd089e55
Bart Frenk <bart.frenk@gmail.com>
2019-07-03 20:04:42 +0200

    Clean up yaml-conf

--- yaml-conf/src/Data/Config/Internals.hs
+++ yaml-conf/src/Data/Config/Internals.hs
@@ -1,10 +1,17 @@
+{-# LANGUAGE DeriveFunctor        #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE MultiWayIf #-}
 module Data.Config.Internals where
 
 import           Control.Monad.Catch       (Exception, MonadThrow, throwM)
+import           Control.Monad.Except
 import           Control.Monad.Trans
 import           Control.Monad.Trans.Maybe
 import           Data.ByteString.Lazy      (ByteString)
+import           Data.Either
 import           Data.Maybe
+import           Data.Monoid
 import           Data.YAML
 import           Data.YAML.Event           (Tag, mkTag)
 
@@ -13,17 +20,21 @@ newtype ParseError = ParseError String deriving (Eq, Show)
 instance Exception ParseError
 
 
-readConfig :: (MonadIO m, MonadThrow m, FromYAML v) => FilePath -> m v
+readConfig :: (MonadIO m, FromYAML v) => FilePath -> ErrT m v
 readConfig = undefined
 
-decodeConfig :: (MonadThrow m, FromYAML v) => ByteString -> m v
+decodeConfig :: (Monad m, MonadIO m, MonadThrow m, FromYAML v) => ByteString -> m v
 decodeConfig bs = case decodeNode bs of
   Left description -> throwM $ ParseError description
   Right [Doc v] -> do
-    node <- resolve baseResolver v
-    case parseEither $ parseYAML node of
-      Left description -> throwM $ ParseError description
-      Right v          -> pure v
+    node' <- runExceptT (resolve baseResolver v)
+    -- Make this more concise: abstract over something like Either String a -> (MonadThrow m => m a)
+    case node' of
+      Left err -> throwM $ ParseError err
+      Right node ->
+        case parseEither $ parseYAML node of
+          Left description -> throwM $ ParseError description
+          Right v          -> pure v
   Right _ -> throwM $ ParseError "Expected a single YAML value"
 
 test :: IO Node
@@ -33,28 +44,59 @@ test = decodeConfig example
               \port: 123\n\
               \password: !secret password"
 
--- Not going to work, since we might need to query the environment to know whether
--- a value exists
-type Resolver m = (Node -> m (Maybe Node)) -> Node -> m (Maybe Node)
+data ResultT m a
+  = Resolved (m (Maybe a))
+  | NoMatch
+  deriving (Functor)
+
+
+defaultResolver :: Monad m => Resolver m
+defaultResolver = orResolver <> baseResolver
+
+type ErrT m a = ExceptT String m a
+
+newtype Resolver m =
+  Resolver ((Node -> ErrT m Node) -> Node -> Maybe (ErrT m Node))
+
+-- envResolver :: MonadIO m => Resolver m
+-- envResolver cont node@(Scalar (SUnknown tag text))
+--   | if tag == "!env" =
 
 orResolver :: Monad m => Resolver m
-orResolver cont node@(Sequence tag nodes)
-  | tag == mkTag "!or" = head <$> cont `mapM` nodes
-  | otherwise = pure Nothing
+orResolver = Resolver orResolver'
   where
-    firstValue [] = undefined
+
+    orResolver' cont node@(Sequence tag nodes) =
+      if | tag == mkTag "!or" -> Just . ExceptT $ do
+             results <- (runExceptT . cont) `mapM` nodes
+             case firstSuccess results of
+               Nothing -> pure $ Left "No successful resolver for !or tag"
+               Just a  -> pure $ Right a
+         | otherwise -> Nothing
+    orResolver' _ node = Nothing
+
+    firstSuccess xs = case dropWhile isLeft xs of
+      []          -> Nothing
+      (Right x:_) -> Just x
+
+instance Semigroup (Resolver m) where
+  (Resolver f) <> (Resolver g) = Resolver $ \cont node ->
+    case f cont node of
+      Just n  -> Just n
+      Nothing -> g cont node
 
 baseResolver :: Monad m => Resolver m
-baseResolver cont node@(Scalar _) = pure $ Just node
-baseResolver cont (Mapping tag mapping) =
-  Just . Mapping tag . collapse <$> cont `mapM` mapping
-baseResolver cont (Sequence tag nodes) =
-  Just . Sequence tag . collapse <$> cont `mapM` nodes
-baseResolver _ node = pure $ Just node
+baseResolver = Resolver baseResolver'
+  where
 
-resolve :: MonadThrow m => Resolver m -> Node -> m (Maybe Node)
-resolve res node = res (resolve res) node
+    baseResolver' cont node@(Scalar _) = Just (pure node)
+    baseResolver' cont (Mapping tag mapping) =
+      Just (Mapping tag <$> cont `mapM` mapping)
+    baseResolver' cont (Sequence tag nodes) =
+      Just (Sequence tag <$> cont `mapM` nodes)
+    baseResolver' _ node = Just (pure node)
 
--- Generalizes catMaybes
-collapse :: Foldable f => f (Maybe a) -> f a
-collapse = undefined
+resolve :: MonadThrow m => Resolver m -> Node -> ErrT m Node
+resolve res@(Resolver f) node = fromMaybe error (f (resolve res) node)
+  where
+    error = throwError "Failed to resolve"
